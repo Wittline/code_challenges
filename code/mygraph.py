eed4f435456c18decc26247a131a35696bccb660
Wittline <8701464+Wittline@users.noreply.github.com>
2022-06-23 23:31:06 -0500

    Update mygraph.py

--- code/mygraph.py
+++ code/mygraph.py
@@ -191,6 +191,32 @@ class myGraph:
         return stack
 
 
+    def __dfs_cycle(self, source, visited, parent = None):
+
+        visited[source] = True
+
+        for vertex in self._graph_dict[source]:
+
+            if not visited[vertex]:
+                if self.__dfs_cycle(vertex, visited, source):
+                    return True
+            elif vertex != parent:
+                return True
+            
+        return False
+
+    
+    def has_cycle(self):
+
+        visited = {k:False for k in self._graph_dict}
+
+        return self.__dfs_cycle("a", visited)
+
+
+
+
+
+
 
 
 
