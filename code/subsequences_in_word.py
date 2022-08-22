184d56f64b2d341aff59cd59a6c5d1462320aeca
Wittline <8701464+Wittline@users.noreply.github.com>
2022-08-22 13:24:43 -0500

    Create subsequences_in_word.py

---
+++ code/subsequences_in_word.py
@@ -0,0 +1,22 @@
+def isSubsequence(word, s):    
+    n = len(word)
+    m = len(s)
+    i = 0
+    j = 0
+
+    while j < m and i < n:
+        if word[j] == s[i]:
+            j = j + 1
+        i = i + 1
+    
+    return j == m
+
+words = []
+s = ""
+count = 0
+def count_words_subsequence(words, s):
+    
+    for word in words:
+        if isSubsequence(word, s):
+            count += 1
+    return count
