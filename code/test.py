057e4d7f522ae0dbd821399d7c5cf6e6182029e6
Wittline <8701464+Wittline@users.noreply.github.com>
2022-08-25 18:59:57 -0500

    Duplicates and plus one

--- code/test.py
+++ code/test.py
@@ -1,14 +1,41 @@
 from mygraph import myGraph
+import dfs_examples
 
-g = { "a" : {"d"},
-      "b" : {"c"},
-      "c" : {"b", "c", "d", "e"},
-      "d" : {"a", "c"},
-      "e" : {"c"},
-      "f" : {}
-    }
 
-graph = myGraph(g)
+# g = { "a" : {"d"},
+#       "d" : {"c"},
+#       "c" : {"f"},
+#       "d" : {"f"},
+#       "f" : {}
+#     }
 
-print(graph.all_vertices())
-print(graph.all_edges())
+# gph = myGraph(g)
+# print(gph.isDag())
+# print(gph.topological_sort())
+
+# mat = [
+#     ['D', 'E', 'M', 'X', 'B'],
+#     ['A', 'O', 'X', 'P', 'E'],
+#     ['X', 'X', 'C', 'X', 'D'],
+#     ['X', 'D', 'X', 'D', 'S'],
+#     ['C', 'O', 'Y', 'E', 'N']
+# ]
+
+# word = 'CODE'
+
+# dfs_examples.findAllOccurences(mat, word)
+
+
+board = [
+    ['M', 'S', 'E', 'F'],
+    ['R', 'A', 'T', 'D'],
+    ['L', 'O', 'N', 'E'],
+    ['K', 'A', 'F', 'B']
+]
+
+words = ['START', 'NOTE', 'SAND', 'STONED']
+
+dfs_examples.searchInBoggle(board, words)
+
+
+
