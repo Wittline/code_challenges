03e4449a90fe132500563fac8ab6006d6b2ef538
Wittline <8701464+Wittline@users.noreply.github.com>
2022-08-27 23:58:09 -0500

    Updating

---
+++ code/arrays/longest_consec_seq.py
@@ -0,0 +1,14 @@
+def longestConsecutiveSeq(nums):
+    numSet = set(nums)
+    longest = 0
+
+    for n in nums:
+
+        if (n - 1) not in numSet:
+            length = 0
+            while (n + length) in numSet:
+                length +=1
+            
+            longest = max(length, longest)
+    
+    return longest
